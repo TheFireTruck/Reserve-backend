d9ded96915ef97e2da8903ac5011fad6039147b6
CozyBrian <dragonbrian33@gmail.com>
2024-03-09 00:09:01 +0000

    remove unnecesary fields

--- db/models/users.py
+++ db/models/users.py
@@ -28,26 +28,3 @@ class User(Base):
 
     organisation_id = Column(UUID() , ForeignKey("organisations.id"),nullable=True)
     organisation = relationship("Organisation", back_populates="users")
-
-    github_profile = Column(String)
-    twitter_profile = Column(String)
-    linkedin_profile = Column(String)
-    portfolio_url = Column(String)
-    profile_pic_url = Column(String)
-    stack_id = Column(Integer, ForeignKey("stacks.id"), nullable=True)
-    created_at = Column(
-        TIMESTAMP(timezone=True),
-        nullable=False, server_default=text('now()'))
-    updated_at = Column(
-        TIMESTAMP(timezone=True),
-        nullable=False, server_default=text('now()'), server_onupdate=text("now()"))
-    is_active = Column(Boolean)
-    status = Column(SQLAlchemyEnum(UserStatus), default=UserStatus.TO_CONTACT, nullable=False)
-
-    skills = relationship('Skill', secondary="users_skills", back_populates='users')
-    role = relationship("Role",  back_populates="users")
-    tags = relationship('Tag', secondary='users_tags', back_populates='users')
-    stack = relationship("Stack", back_populates='users')
-    projects = relationship("Project", secondary="users_projects", back_populates="members")
-    managed_projects = relationship("Project", back_populates="manager")
-    technical_task_submission = relationship("TechnicalTaskSubmission", back_populates="user")
